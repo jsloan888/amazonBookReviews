49d434a9fad029a64172232e5848187a9cbfc063
jsloan888 <jsloan888@pm.me>
2021-02-01 18:59:38 -0500

    added html to templates

--- amazonBookReviews/wsgi.py
+++ amazonBookReviews/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Book_review project.
+WSGI config for amazonBookReviews project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Book_review.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amazonBookReviews.settings')
 
 application = get_wsgi_application()
